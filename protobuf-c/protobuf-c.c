d6acedd733538270f84174b565ab1269f6e02116
openvela-robot <openvela-robot@xiaomi.com>
2020-06-20 15:26:19 +0800

    Merge pull request #427 from protobuf-c/ilya-oneof
    c_message.cc: Resolve name conflict (Fixes #389)

--- protobuf-c/protobuf-c.c
+++ protobuf-c/protobuf-c.c
@@ -1618,19 +1618,22 @@ required_field_pack_to_buffer(const ProtobufCFieldDescriptor *field,
 		break;
 	}
 	case PROTOBUF_C_TYPE_MESSAGE: {
+		uint8_t simple_buffer_scratch[256];
+		size_t sublen;
 		const ProtobufCMessage *msg = *(ProtobufCMessage * const *) member;
-		
+		ProtobufCBufferSimple simple_buffer =
+			PROTOBUF_C_BUFFER_SIMPLE_INIT(simple_buffer_scratch);
+
 		scratch[0] |= PROTOBUF_C_WIRE_TYPE_LENGTH_PREFIXED;
-		if (msg == NULL) {
-			rv += uint32_pack(0, scratch + rv);
-			buffer->append(buffer, rv, scratch);
-		} else {
-			size_t sublen = protobuf_c_message_get_packed_size(msg);
-			rv += uint32_pack(sublen, scratch + rv);
-			buffer->append(buffer, rv, scratch);
-			protobuf_c_message_pack_to_buffer(msg, buffer);
-			rv += sublen;
-		}
+		if (msg == NULL)
+			sublen = 0;
+		else
+			sublen = protobuf_c_message_pack_to_buffer(msg, &simple_buffer.base);
+		rv += uint32_pack(sublen, scratch + rv);
+		buffer->append(buffer, rv, scratch);
+		buffer->append(buffer, sublen, simple_buffer.data);
+		rv += sublen;
+		PROTOBUF_C_BUFFER_SIMPLE_CLEAR(&simple_buffer);
 		break;
 	}
 	default:
@@ -2092,9 +2095,9 @@ parse_tag_and_wiretype(size_t len,
 
 /* sizeof(ScannedMember) must be <= (1UL<<BOUND_SIZEOF_SCANNED_MEMBER_LOG2) */
 #define BOUND_SIZEOF_SCANNED_MEMBER_LOG2 5
-typedef struct ScannedMember ScannedMember;
+typedef struct _ScannedMember ScannedMember;
 /** Field as it's being read. */
-struct ScannedMember {
+struct _ScannedMember {
 	uint32_t tag;              /**< Field tag. */
 	uint8_t wire_type;         /**< Field type. */
 	uint8_t length_prefix_len; /**< Prefix length. */
@@ -2129,13 +2132,11 @@ scan_length_prefixed_data(size_t len, const uint8_t *data,
 		// Protobuf messages should always be less than 2 GiB in size.
 		// We also want to return early here so that hdr_len + val does
 		// not overflow on 32-bit systems.
-		PROTOBUF_C_UNPACK_ERROR("length prefix of %lu is too large",
-			(unsigned long int)val);
+		PROTOBUF_C_UNPACK_ERROR("length prefix of %lu is too large", val);
 		return 0;
 	}
 	if (hdr_len + val > len) {
-		PROTOBUF_C_UNPACK_ERROR("data too short after length-prefix of %lu",
-			(unsigned long int)val);
+		PROTOBUF_C_UNPACK_ERROR("data too short after length-prefix of %lu", val);
 		return 0;
 	}
 	return hdr_len + val;
